e3e169a0b873a9cff971050bfe45e7ed22664816
Michael Elsdörfer <michael@elsdoerfer.com>
2011-08-12 20:38:00 +0200

    Increment version number.

--- src/flaskext/assets.py
+++ src/flaskext/assets.py
@@ -5,7 +5,7 @@ from webassets import Bundle
 from webassets.env import BaseEnvironment, ConfigStorage
 
 
-__version__ = (0, 3, 'dev')
+__version__ = (0, 5, '1')
 
 __all__ = ('Environment', 'Bundle',)
 
