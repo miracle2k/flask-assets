e972588747377b7af1aea86103acc47c5a748360
Michael Elsdörfer <michael@elsdoerfer.com>
2012-03-10 23:46:03 +0100

    Alright, this should make setup.py work again.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Integrates the ``webassets`` library with Flask, adding support for
 merging, minifying and compiling CSS and Javascript files.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # Figure out the version; this could be done by importing the
 # module, though that requires dependencies to be already installed,
@@ -40,7 +40,8 @@ setup(
     description='Asset management for Flask, to compress and merge ' \
         'CSS and Javascript files.',
     long_description=__doc__,
-    packages=find_packages('src'),
+    py_modules=['flask_assets'],
+    package_dir={'': 'src'},
     zip_safe=False,
     platforms='any',
     install_requires=[
