95b8c99d82de04b965256d07011039a189d0cd30
Michael Elsdörfer <michael@elsdoerfer.com>
2014-02-20 21:47:59 +0100

    Move to new dev version.

--- src/flask_assets.py
+++ src/flask_assets.py
@@ -8,8 +8,8 @@ from webassets.filter import Filter, register_filter
 from webassets.loaders import PythonLoader, YAMLLoader
 
 
-__version__ = (0, 9)
-__webassets_version__ = (0, 9)  # webassets core compatibility. used in setup.py
+__version__ = (0, 10, 'dev')
+__webassets_version__ = ('dev',)  # webassets core compatibility. used in setup.py
 
 
 __all__ = ('Environment', 'Bundle',)
